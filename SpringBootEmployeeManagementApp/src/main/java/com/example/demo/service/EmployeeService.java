5625af576c01dc3c1b3b2371dc099e0c58995f64
Devansh Upadhyay <upadhyay.devansh29@gmail.com>
2025-01-23 11:00:53 +0530

    First Commit

---
+++ SpringBootEmployeeManagementApp/src/main/java/com/example/demo/service/EmployeeService.java
@@ -0,0 +1,7 @@
+package com.example.demo.service;
+
+import java.util.Date;
+
+public interface EmployeeService {
+	void add(int empId, String empName, int empSal, String empDesg, Date empDOJ);
+}
---
+++ SpringBootEmployeeManagementApp/src/main/java/com/example/demo/service/EmployeeServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.demo.service;
+
+import java.util.Date;
+
+public class EmployeeServiceImpl implements EmployeeService {
+
+	@Override
+	public void add(int empId, String empName, int empSal, String empDesg, Date empDOJ) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
